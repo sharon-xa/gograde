dd6a7c57ffe0c73b89ffbc739e672707890d0419
sharon <ali93456@gmail.com>
2026-05-09 20:49:14 +0300

    pass upgraders to the runner based on the OS

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -40,17 +40,8 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		runner.Run(context.Background(), []upgraders.Upgrader{
-			upgraders.NewApt(),
-			upgraders.NewPacman(),
-			upgraders.NewYay(),
-			upgraders.NewBrew(),
-			upgraders.NewDnf(),
-			upgraders.NewFlatpak(),
-			upgraders.NewNpm(),
-			upgraders.NewParu(),
-			upgraders.NewWinget(),
-		})
+		upgraderList := upgraders.GetUpgradersBasedOnOS()
+		runner.Run(context.Background(), upgraderList)
 	},
 }
 
--- internal/upgraders/upgrader.go
+++ internal/upgraders/upgrader.go
@@ -2,11 +2,40 @@
 // which will implement the interface below
 package upgraders
 
-import "context"
+import (
+	"context"
+
+	"github.com/sharon-xa/gograde/internal/utils"
+)
 
 type Upgrader interface {
 	Name() string
 	Available() bool
 	Privileged() bool
 	Run(ctx context.Context) error
+}
+
+func GetUpgradersBasedOnOS() []Upgrader {
+	if utils.IsWindows() {
+		return []Upgrader{
+			NewWinget(),
+			NewScoop(),
+			NewChoco(),
+
+			NewNpm(),
+		}
+	}
+
+	return []Upgrader{
+		NewApt(),
+		NewBrew(),
+		NewDnf(),
+		NewFlatpak(),
+		NewPacman(),
+		NewParu(),
+		NewYay(),
+		NewPort(),
+
+		NewNpm(),
+	}
 }
